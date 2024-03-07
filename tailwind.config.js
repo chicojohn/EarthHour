ec71bb9d2b008e01d2a6ae359824816356124f13
Mangayao <114972538+chicojohn@users.noreply.github.com>
2024-03-07 14:01:11 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        violet : "#8a2be2",
+      },
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'],
+      },
+    },
+    
+  },
+  plugins: [],
+}
+
